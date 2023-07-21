2d433104dffe91ffc519ab9340932052944fe3a7
Sumit Parida <80693203+sumitparida2002@users.noreply.github.com>
2023-07-20 23:43:22 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Movie Library`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
